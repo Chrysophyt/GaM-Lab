fef97eb17cf7b63bad90726160b09efb6c7e43d6
Chrysophyt <ian240201@gmail.com>
2019-03-05 20:48:07 +0700

    Basic Header Footer

---
+++ GaM-Lab-alpha/src/app/toolbar/footbar.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'footbar',
+  templateUrl: 'footbar.html',
+  styleUrls: ['footbar.css'],
+})
+export class footbar {}
--- GaM-Lab-alpha/src/main.ts
+++ GaM-Lab-alpha/src/main.ts
@@ -1,12 +1,36 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'hammerjs';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material-module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+import {toolbar} from './app/toolbar/toolbar';
+import {footbar} from './app/toolbar/footbar';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  entryComponents: [toolbar, footbar],
+  declarations: [toolbar, footbar],
+  bootstrap: [toolbar, footbar],
+  providers: []
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
